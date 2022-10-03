d7d23b05aab4bcf533a9cb2bee7ef13c22f65606
Nilesh <das.nilesh72@gmail.com>
2022-10-03 21:50:49 +0530

    styleguide css

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -5,16 +5,17 @@ import LOGO from "../../SVGImages/logoDark.svg";
 export default function Header() {
   return (
     <div className='header'>
-        <div>
-            <img src={LOGO} className="logo"/>
-        </div>
-        <div>
+          <img src={LOGO} className="logo"/>
           <ul className='header-nav'>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
             <li>About</li>
             <li>Portfolio</li>
             <li>Contact</li>
+            <li className='header-button'>Button</li>
           </ul>
-        </div>
     </div>
   )
 }
