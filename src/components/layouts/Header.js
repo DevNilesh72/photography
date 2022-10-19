33a5f09bcdf11696ec663013b9bf3b160cf71230
Nilesh <das.nilesh72@gmail.com>
2022-10-19 21:20:21 +0530

    header css fix

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 import "../../styles/header.css";
 import LOGO from "../../SVGImages/logoDark.svg";
 // https://css-tricks.com/css-link-hover-effects/#aa-the-rainbow-underline-link-hover-effect
 export default function Header() {
   return (
-    <div className='header'>
-          <img src={LOGO} className="logo"/>
-          <ul className='header-nav'>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>About</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-            <li className='header-button'>Button</li>
-          </ul>
+    <div className="header">
+      <section className="container">
+        <img src={LOGO} className="logo" />
+        <ul className="header-nav">
+          <li className="nav-element">Home</li>
+          <li className="nav-element">About</li>
+          <li className="nav-element">Portfolio</li>
+          <li className="nav-element">Contact</li>
+          <li className="nav-element">Home</li>
+          <li className="nav-element">About</li>
+          <li className="primary-cta">Button</li>
+        </ul>
+      </section>
     </div>
-  )
+  );
 }
