a5ef0b31f55036e3251b4d7d46e17e63b0561e6d
Nilesh <das.nilesh72@gmail.com>
2022-10-29 18:38:53 +0530

    carousel init

--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Carousel from '../sections/carousel'
 
 export default function Home() {
   return (
     <div className='main-content'>
+      <Carousel />
     </div>
   )
 }
---
+++ src/components/sections/carousel.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../styles/home.css'
+
+export default function Carousel() {
+  return (
+    <div className='carousel'>carousel</div>
+  )
+}
